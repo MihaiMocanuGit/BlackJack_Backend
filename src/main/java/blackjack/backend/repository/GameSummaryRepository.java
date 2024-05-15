bee74cc88cea28c95b771893f68bbc997d30d70e
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2024-05-15 19:59:15 +0300

    defined repo

--- src/main/java/blackjack/backend/repository/GameSummaryRepository.java
+++ src/main/java/blackjack/backend/repository/GameSummaryRepository.java
@@ -3,9 +3,11 @@ package blackjack.backend.repository;
 import blackjack.backend.domain.GameSummary;
 import blackjack.backend.domain.Player;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface GameSummaryRepository extends MongoRepository<GameSummary, String> {
     public List<GameSummary> findGameSummariesByPlayer(Player player);
     public List<GameSummary> findGameSummariesByPlayer_Uid(String playerUid);
--- src/main/java/blackjack/backend/repository/PlayerRepository.java
+++ src/main/java/blackjack/backend/repository/PlayerRepository.java
@@ -3,9 +3,12 @@ package blackjack.backend.repository;
 import blackjack.backend.domain.Player;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+
+@Repository
 public interface PlayerRepository extends MongoRepository<Player, String>{
     public List<Player> findByUsername(String username);
     public List<Player> findByLevelBetween(float leve1, float level2);
